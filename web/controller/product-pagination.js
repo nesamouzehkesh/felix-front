0ee84824602aa0f1fb341d8817aa224a3c3fdb00
nesamouzehkesh <n.mouzehkesh@gmail.com>
2016-04-11 22:11:01 +1000

    Delete product action done

--- web/controller/product-pagination.js
+++ web/controller/product-pagination.js
@@ -2,19 +2,31 @@
 
 var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination','StoryServices']);
 
-    myApp.controller('MyController', ['$scope', 'ProductRepository', function ($scope, ProductRepository) {
-      $scope.currentPage = 1;
-      $scope.pageSize = 10;
-      $scope.products = [];
+    myApp.controller('MyController', ['$scope', '$http', 'ProductRepository', function ($scope, $http, ProductRepository) {
+        $scope.currentPage = 1;
+        $scope.pageSize = 10;
+        $scope.products = [];
 
-      ProductRepository.getAllProducts()
-        .success(function (data) {
-            $scope.products = data;
-        }); 
+        ProductRepository.getAllProducts()
+          .success(function (data) {
+              $scope.products = data;
+          }); 
 
-      $scope.pageChangeHandler = function(num) {
-          console.log('meals page changed to ' + num);
-      };
+        $scope.pageChangeHandler = function(num) {
+            console.log('meals page changed to ' + num);
+        };
+
+        $scope.deleteItem = function (itemId) {
+            ProductRepository.deleteProduct(itemId)
+                .success(function (data) {
+                    for (var i = 0; i < $scope.products.length; i++ ) {
+                        if( $scope.products[i].id === itemId ) {
+                            $scope.products.splice(i, 1 );     
+                            break;
+                        }
+                    }              
+            }); 
+        };
     }]);
 
     myApp.controller('OtherController', ['$scope', function ($scope) {
@@ -22,5 +34,4 @@ var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination','St
         console.log('going to page ' + num);
       };
     }]);
-
 })();
